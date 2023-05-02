aeb05c40be63c576a12e3b256dd6dcedddc48647
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-02 08:09:52 +0300

    Змінено завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,16 @@ const ingredients = [
   'Condiments',
 ];
 
-for (const ingredient of ingredients) {
-  const liEl = document.createElement("li");
-  liEl.textContent = ingredient;
-  liEl.classList.add("item");
-  const ulIngredients = document.querySelector("#ingredients");
-  ulIngredients.appendChild(liEl);
-}
+// for (const ingredient of ingredients) {
+//   const liEl = document.createElement("li");
+//   liEl.textContent = ingredient;
+//   liEl.classList.add("item");
+//   const ulIngredients = document.querySelector("#ingredients");
+//   ulIngredients.appendChild(liEl);
+// }
+const ulIngredients = document.querySelector("#ingredients");
+const imgEl = images
+  .map((image) => `<li><img src=${image.url} alt=${image.alt}></img></li>`)
+  .join("");
+
+list.insertAdjacentHTML("beforeend", imgEl);
